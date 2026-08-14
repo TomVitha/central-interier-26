3c0c199e9dfed3b75fa5d2d165cb55cba1888b6c
Tom Vitha <tomasvitha01@gmail.com>
2026-08-14 14:20:48 +0200

    idk stuff

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
-/* ! IMPORTANT: Removes default app styling ! */
+/* ! IMPORTANT: Removes CG Vue styles ! */
 document.querySelectorAll('link:is([href*="app."], [href*="chunk-vendors"], [href*="footer-cg"])[href*=".css"]').forEach((element) => {
   console.warn("Removing element:", element)
   element.remove()
 })
 
 
-  /**
-   * HEADER
-   */
-  (() => {
-    const header = document.querySelector("header");
-    const headerSubmenus = header?.querySelectorAll(".header-submenu") ?? [];
+/**
+ * HEADER
+ */
+document.addEventListener("click", (event) => {
+  const header = document.querySelector("header");
+  const headerSubmenus = header?.querySelectorAll(".header-submenu") ?? [];
 
-    document.addEventListener("click", (event) => {
-      if (event.target.closest(".header-submenu")) return;  // Do not close/open on click inside submenu
+  if (event.target.closest(".header-submenu")) return;  // Do not close/open on click inside submenu
 
-      const currentSubmenu = event.target.closest(".header-link")?.nextElementSibling;
-      const isCurrentSubmenuOpen = currentSubmenu?.classList.contains("open");
+  const currentSubmenu = event.target.closest(".header-link")?.nextElementSibling;
+  const isCurrentSubmenuOpen = currentSubmenu?.classList.contains("open");
 
-      headerSubmenus.forEach((menu) => menu.classList.remove("open"));
+  headerSubmenus.forEach((menu) => menu.classList.remove("open"));
 
-      if (currentSubmenu && !isCurrentSubmenuOpen) {
-        currentSubmenu.classList.add("open");
-      }
-    });
-  })()
+  if (currentSubmenu && !isCurrentSubmenuOpen) {
+    currentSubmenu.classList.add("open");
+  }
+});
